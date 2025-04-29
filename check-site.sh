76435fba17c83c9456faef44e545aa01aa46d8c9
Sabeer.Zubair <sabeer.zubair@IT-DV-046-M.local>
2025-04-29 12:32:38 +0300

    test deploy

--- check-site.sh
+++ check-site.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #check the http status code
-STATUS=`curl -is --connect-timeout 3 http://13.201.164.185:81/ | head -1 | awk '{print $2}'`
+STATUS=`curl -is --connect-timeout 3 http://13.201.164.185:82/ | head -1 | awk '{print $2}'`
 if test $STATUS -eq 200; then
   echo "We're Good - $STATUS OK!"
 else
   echo "ERROR: $STATUS"
-exit 1
+  exit 1
 fi
